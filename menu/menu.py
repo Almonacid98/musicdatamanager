ff0e2aa4befdb59e84bb6fd8c69dde1621eee8d1
Almonacid98 <gabriel98almonacid@hotmail.com>
2025-12-10 18:18:20 -0300

    Creación de estructura del proyecto y armado de menú básico.

---
+++ menu/menu.py
@@ -0,0 +1,36 @@
+from utils.colors import *
+def menu():
+
+    file_path = "data/spotify_and_youtube_2024.csv"
+    #songs = load_songs(file_path)
+
+    while True:
+        print(f"\n{CYAN}========== MENÚ PRINCIPAL =========={RESET}")
+        print(f"{YELLOW}1 - Buscar por título o artista{RESET}")
+        print(f"{YELLOW}2 - Mostrar Top 10 con mayor popularidad (reproducciones) por artista{RESET}")
+        print(f"{YELLOW}3 - Insertar registro{RESET}") 
+        print(f"{YELLOW}4 - Información de artista{RESET}")
+        print(f"{YELLOW}5 - Salir{RESET}")
+        print(f"{CYAN}===================================={RESET}")
+
+        opcion = input("Seleccione una opción: ")
+
+        if opcion == "1":
+            pass
+
+        elif opcion == "2":
+            pass
+
+        elif opcion == "3":
+            pass
+
+        elif opcion == "4":
+            pass 
+
+        elif opcion == "5":
+            print("Saliendo...")
+            break
+
+        else:
+            print("Opción inválida.")
+
