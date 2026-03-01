77815833dafce9aff7ab19177883a82468deb4db
Almonacid98 <gabriel98almonacid@hotmail.com>
2026-02-28 21:36:43 -0300

    Se mejoró el método apeend_row que lee el header o cabecera del csv, completa campos faltantes con 0, con esto aseguro que ya no tengo datos que no pertenecen a sus columnas, además de si faltan datos de header

--- utils/csv_utils.py
+++ utils/csv_utils.py
@@ -14,11 +14,16 @@ def find_last_index(file_path):
     except FileNotFoundError:
         return 1
 
+def append_row(file_path, data_dict):
 
-def append_row(file_path, row):
-    with open(file_path, 'a', newline='', encoding='utf-8') as file:
-        writer = csv.DictWriter(file, fieldnames=row.keys())
-        file.seek(0, 2)
-        if file.tell() == 0:
-            writer.writeheader()
-        writer.writerow(row)
+    # Leer el header real del archivo
+    with open(file_path, 'r', encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+        fieldnames = reader.fieldnames
+
+    # Completar cualquier campo faltante con "0"
+    complete_row = {field: data_dict.get(field, "0") for field in fieldnames}
+
+    with open(file_path, 'a', newline='', encoding='utf-8') as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writerow(complete_row)
