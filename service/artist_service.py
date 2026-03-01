acd05f1238bc198d115f86ad5ba1356ee92c9567
Almonacid98 <gabriel98almonacid@hotmail.com>
2026-03-01 18:04:22 -0300

    borre lineas

--- service/artist_service.py
+++ service/artist_service.py
@@ -1,13 +1,11 @@
 def get_artist_album_info(artista, registros):
     albums = {}
     real_artist_name = None
-
     for registro in registros:
         artist_name = registro.get('Artist', '').strip()
 
         if artista.lower() in artist_name.lower():
-
-            # Guardamos el nombre real la primera vez
+            # Guardo nombre 
             if real_artist_name is None:
                 real_artist_name = artist_name
 
@@ -19,8 +17,6 @@ def get_artist_album_info(artista, registros):
                     'canciones': 0,
                     'duracion_total': 0
                 }
-
             albums[album]['canciones'] += 1
             albums[album]['duracion_total'] += duracion
-
     return real_artist_name, albums
